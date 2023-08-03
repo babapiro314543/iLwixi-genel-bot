129adfa5352523620486db24978f98011d9acabd
babapiro314543 <140435654+babapiro314543@users.noreply.github.com>
2023-08-03 14:27:02 +0300

    iLwixi genel bot
    Genel bot

---
+++ v14-genel-bot/events/ready.js
@@ -0,0 +1,9 @@
+const { ActivityType, Events } = require("discord.js")
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+    execute(client) {
+        let activities = [`💖 iLwixi bot yenilendi !.`, `${client.user.username}` , `🎈 iLwixi Bot v2.0`], i = 0;
+        setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
+    }
+};
